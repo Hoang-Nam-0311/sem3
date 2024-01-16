c7c5d92d5a7c551695d9afceaf4af7f4c843b485
Hoang-Nam-0311 <nguyennam031123@gmail.com>
2024-01-16 15:51:43 +0700

    ok

---
+++ Project-SemIII/Controllers/CerrerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_SemIII.Controllers
+{
+    public class CerrerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
